2a7cb54fd833ad808b87c6d3f02e208e813ae542
Tony Sheng <33916962+alonehat@users.noreply.github.com>
2019-07-09 14:45:33 +0800

    Update ispread.js
    make it better

--- ispread.js
+++ ispread.js
@@ -1,18 +1,12 @@
 $(".spread_container").find(".ispread_main").hover(function () {
-    var objs = $(this).parent().find(".ispread");
-    var basePercent = 25;
-    for (var i = 0, p = objs.length; i < p; i++) {
-        var percent = [basePercent, "%"].join("");
-
-        objs.eq(i).stop().animate({ marginLeft: percent }, 800);
-
-        basePercent += 25;
-    }
-
-}, function () {
-    var objs = $(this).parent().find(".ispread");
-
-    for (var i = 0, p = objs.length; i < p; i++) {
-        objs.eq(i).stop().animate({ marginLeft: "0%" }, 800);
-    }
-});
+            var objs = $(this).parent().find(".ispread");
+            for (var i = 0, p = objs.length; i < p; i++) {
+                var percent = [(100/(p+1)) * (i + 1), "%"].join("");
+                objs.eq(i).stop().animate({ marginLeft: percent }, 800);
+            }
+        }, function () {
+            var objs = $(this).parent().find(".ispread");
+            for (var i = 0, p = objs.length; i < p; i++) {
+                objs.eq(i).stop().animate({ marginLeft: "0%" }, 800);
+            }
+        });
