a90f6d69d056d840007e0f7d2f1c78869557b5ab
Tony Sheng <33916962+alonehat@users.noreply.github.com>
2019-07-09 15:08:59 +0800

    Update ispread.js
    after click,keep status now if not click before

--- ispread.js
+++ ispread.js
@@ -1,12 +1,22 @@
-$(".spread_container").find(".ispread_main").hover(function () {
+ $(".spread_container").find(".ispread_main").hover(function () {
             var objs = $(this).parent().find(".ispread");
             for (var i = 0, p = objs.length; i < p; i++) {
-                var percent = [(100/(p+1)) * (i + 1), "%"].join("");
+                var percent = [(100 / (p + 1)) * (i + 1), "%"].join("");
                 objs.eq(i).stop().animate({ marginLeft: percent }, 800);
             }
         }, function () {
-            var objs = $(this).parent().find(".ispread");
-            for (var i = 0, p = objs.length; i < p; i++) {
-                objs.eq(i).stop().animate({ marginLeft: "0%" }, 800);
+            if (!$(this).hasClass("gotya")) {
+                var objs = $(this).parent().find(".ispread");
+                for (var i = 0, p = objs.length; i < p; i++) {
+                    objs.eq(i).stop().animate({ marginLeft: "0%" }, 800);
+                }
+            }
+        });
+
+        $(".spread_container").find(".ispread_main").on("click", function () {
+            if ($(this).hasClass("gotya")) {
+                $(this).removeClass("gotya");
+            } else {
+                $(this).addClass("gotya");
             }
         });
